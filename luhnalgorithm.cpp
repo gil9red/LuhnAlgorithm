df1bd576f3ca229bda32301f0da8ba069b84c526
gil9red <gil9red@gmail.com>
2014-04-29 01:00:31 +0600

    Рефакторинг Перегрузка функции execute
    Signed-off-by: gil9red <gil9red@gmail.com>

--- luhnalgorithm.cpp
+++ luhnalgorithm.cpp
@@ -9,19 +9,24 @@ const int getSumSequence( const QVector < qint16 > & arr )
     return sum;
 }
 
-LuhnAlgorithm::LuhnAlgorithm( const QString & t )
-    : text(t), sum(0), valid( false )
+LuhnAlgorithm::LuhnAlgorithm( const QString & number )
+    : sum(0), valid( false )
 {
-    text = text.remove( ' ' );
+    setNumberCard( number );
 }
 
 LuhnAlgorithm &LuhnAlgorithm::execute()
 {
-    digits = getSequence( text );
+    digits = getSequence( numberCard );
     sum = getSumSequence( digits );
     valid = sum % 10 == 0; // Если сумма кратна 10, тогда номер правильный
 
     return *this;
+}
+LuhnAlgorithm & LuhnAlgorithm::execute( const QString & number )
+{
+    setNumberCard( number );
+    return execute();
 }
 int LuhnAlgorithm::getSum() const
 {
@@ -32,20 +37,26 @@ bool LuhnAlgorithm::isValid() const
     return valid;
 }
 
+void LuhnAlgorithm::setNumberCard(const QString & number )
+{
+    numberCard = number;
+    numberCard = numberCard.remove( ' ' );
+}
+
 QVector < qint16 > LuhnAlgorithm::getSequence()
 {
     return digits;
 }
-QVector < qint16 > LuhnAlgorithm::getSequence( const QString & text )
+QVector < qint16 > LuhnAlgorithm::getSequence( const QString & numberCard )
 {
     // Алгоритм Луна
     // http://ru.wikipedia.org/wiki/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%9B%D1%83%D0%BD%D0%B0
     //
     QVector < qint16 > array;
-    const qint32 length = text.length();
+    const qint32 length = numberCard.length();
     for( qint32 i = 0; i < length; i++ )
     {
-        qint16 digit = text.at(i).digitValue();
+        qint16 digit = numberCard.at(i).digitValue();
         if ( i % 2 == 0 )
         {
             digit *= 2;
--- luhnalgorithm.h
+++ luhnalgorithm.h
@@ -7,16 +7,19 @@
 class LuhnAlgorithm
 {
 public:
-    LuhnAlgorithm( const QString & t );
+    LuhnAlgorithm( const QString & number = "" );
     LuhnAlgorithm & execute();
+    LuhnAlgorithm & execute(const QString & number );
     int getSum() const;
     bool isValid() const;
 
+    void setNumberCard( const QString & number );
+
     QVector < qint16 > getSequence();
-    static QVector < qint16 > getSequence( const QString & text );
+    static QVector < qint16 > getSequence(const QString & numberCard );
 
 private:
-    QString text;
+    QString numberCard;
     int sum;
     QVector < qint16 > digits;
     bool valid;
